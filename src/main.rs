90161de2b62c420d3535c1e5ca9f132bec459a40
akshatarora130 <akshatarora130@gmail.com>
2025-06-11 20:48:43 +0530

    HD wallet key pairs in sol

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,33 @@
-use ethers::{core::{k256::{EncodedPoint}, rand::thread_rng}, signers::{LocalWallet, Signer}};
-use solana_sdk::{signature::Keypair, signer::Signer as EthSigner};
+use bip39::Language;
+use ed25519_hd_key::derive_from_path;
+use solana_sdk::{signature::Keypair, signer::Signer};
 
-// EdDSA Algorithm used by SOL 
-// Eleptic curve used in this is ed25519
-fn sol_keys() {
-    // Step 1 - Create key pair
-    let key_pair = Keypair::new();
-    
-    let public_key = key_pair.pubkey();
-    let public_key_bytes = public_key.to_bytes();
-    println!("SOL Public Key -> {}", public_key);
-
-    let private_key_bytes = key_pair.secret_bytes();
-    let private_key = bs58::encode(private_key_bytes).into_string();
-    
-    println!("SOL Private Key -> {}", private_key);
-
-    // Step 2 - Define a message to sign
-    let string = String::from("Hello");
-    // Step 3 - Convert the message to bytes
-    let message = string.as_bytes();
-
-    // let wrong_message_string = String::from("testing");
-    // let wrong_message = wrong_message_string.as_bytes();
-
-    // Step 4 - Sign the message
-    let signature = key_pair.sign_message(message);
-    println!("SOL Signature -> {:?}", signature);
-
-    // Step 5 - Verify the message
-    let verify = signature.verify(&public_key_bytes, &message);
-    println!("SOL Verify -> {}", verify);
-}
-
-// ECDSA algorithm used by ETH 
-// Eleptic curve used in this is secp256k1
-async fn eth_keys() {
-    let wallet = LocalWallet::new(&mut thread_rng());
-
-    let wallet_address = wallet.address();
-    println!("ETH Wallet address -> {:?}", wallet_address);
-    
-    let verifying_key = wallet.signer().verifying_key();
-    let pubkey_point: EncodedPoint = verifying_key.to_encoded_point(false); // false = uncompressed
-    let public_key_bytes = pubkey_point.as_bytes();
-    let public_key = bs58::encode(public_key_bytes).into_string();
-    println!("ETH Public Key -> {}", public_key);
-
-    let private_key_bytes = wallet.signer().to_bytes();
-    let private_key = bs58::encode(private_key_bytes).into_string();
-    println!("ETH Private Key -> {}", private_key);
-
-    let message_string = String::from("Hello");
-    let message = message_string.as_bytes(); 
-
-    // let wrong_message_string = String::from("testing");
-    // let wrong_message = wrong_message_string.as_bytes();
-    let signature = wallet.sign_message(message).await;
-
-    match signature {
-        Ok(sign) => {
-            match sign.recover(message) {
-                Ok(recovered_address) => {
-                    println!("ETH Recovered Address -> {:?}", recovered_address);
-                    println!("ETH Signature is valid -> {}", recovered_address == wallet_address);
-                },
-                Err(e) => println!("Failed to recover address: {:?}", e),
-            }
-        }
-        Err(e) => println!("Signing failed: {:?}", e),
-    }
-}
+mod raw_key_pairs;
 
 #[tokio::main]
 async fn main() {
-    sol_keys();
-    eth_keys().await;
+    raw_key_pairs::sol_keys();
+    raw_key_pairs::eth_keys().await;
+
+    let result = bip39::Mnemonic::generate_in(Language::English, 12);
+    match result {
+        Ok(words) =>  {
+            println!("{}", words.to_string());
+
+            let seed = words.to_seed("");
+            println!("Seed -> {:?}", seed);
+
+            for i in 0..1 {
+                let path = format!("m/44'/501'/{}'/0'", i);
+                let derivation_path = derive_from_path(&path, &seed);
+                let secret_key = bs58::encode(derivation_path.0).into_string();
+                let key_pair = Keypair::new_from_array(derivation_path.0);
+                let public_key = key_pair.pubkey();
+
+                println!("Private Key -> {}", secret_key);
+                println!("Pulic Key -> {}", public_key);
+            }
+        }
+        Err(e) => println!("Error in generating words {:?}", e)
+    }
 }
---
+++ src/raw_key_pairs.rs
@@ -0,0 +1,75 @@
+use ethers::{core::{k256::{EncodedPoint}, rand::thread_rng}, signers::{LocalWallet, Signer}};
+use solana_sdk::{signature::Keypair, signer::Signer as EthSigner};
+
+// EdDSA Algorithm used by SOL 
+// Eleptic curve used in this is ed25519
+pub fn sol_keys() {
+    // Step 1 - Create key pair
+    let key_pair = Keypair::new();
+    // This is actual private key that we see in various wallets
+    println!("Key Pair -> {}", key_pair.to_base58_string());
+    
+    let public_key = key_pair.pubkey();
+    let public_key_bytes = public_key.to_bytes();
+    println!("SOL Public Key -> {}", public_key);
+
+    let private_key_bytes = key_pair.secret_bytes();
+    let private_key = bs58::encode(private_key_bytes).into_string();
+    
+    println!("SOL Private Key -> {}", private_key);
+
+    // Step 2 - Define a message to sign
+    let string = String::from("Hello");
+    // Step 3 - Convert the message to bytes
+    let message = string.as_bytes();
+
+    // let wrong_message_string = String::from("testing");
+    // let wrong_message = wrong_message_string.as_bytes();
+
+    // Step 4 - Sign the message
+    let signature = key_pair.sign_message(message);
+    println!("SOL Signature -> {:?}", signature);
+
+    // Step 5 - Verify the message
+    let verify = signature.verify(&public_key_bytes, &message);
+    println!("SOL Verify -> {}", verify);
+}
+
+// ECDSA algorithm used by ETH 
+// Eleptic curve used in this is secp256k1
+pub async fn eth_keys() {
+    let wallet = LocalWallet::new(&mut thread_rng());
+
+    let wallet_address = wallet.address();
+    println!("ETH Wallet address -> {:?}", wallet_address);
+    
+    let verifying_key = wallet.signer().verifying_key();
+    let pubkey_point: EncodedPoint = verifying_key.to_encoded_point(false); // false = uncompressed
+    let public_key_bytes = pubkey_point.as_bytes();
+    let public_key_hex = hex::encode(public_key_bytes);
+    println!("ETH Public Key -> {}", public_key_hex);
+
+    let private_key_bytes = wallet.signer().to_bytes();
+    let private_key = hex::encode(private_key_bytes);
+    println!("ETH Private Key -> 0x{}", private_key);
+    
+    let message_string = String::from("Hello");
+    let message = message_string.as_bytes(); 
+
+    // let wrong_message_string = String::from("testing");
+    // let wrong_message = wrong_message_string.as_bytes();
+    let signature = wallet.sign_message(message).await;
+
+    match signature {
+        Ok(sign) => {
+            match sign.recover(message) {
+                Ok(recovered_address) => {
+                    println!("ETH Recovered Address -> {:?}", recovered_address);
+                    println!("ETH Signature is valid -> {}", recovered_address == wallet_address);
+                },
+                Err(e) => println!("Failed to recover address: {:?}", e),
+            }
+        }
+        Err(e) => println!("Signing failed: {:?}", e),
+    }
+}
