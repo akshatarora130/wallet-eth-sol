7348daf944595b9e056590cfcae8465f304d0daf
akshatarora130 <akshatarora130@gmail.com>
2025-06-13 06:45:07 +0530

    .

---
+++ src/hd_wallet_sol.rs
@@ -0,0 +1,32 @@
+// use std::str::FromStr;
+
+use bip39::Language;
+use ed25519_hd_key::derive_from_path;
+use solana_sdk::{signature::Keypair, signer::Signer};
+
+pub fn generate_n_key_pair(n: u32) {
+    let result = bip39::Mnemonic::generate_in(Language::English, 12);
+    // let my_words = "";
+    // let my_mnemonic = bip39::Mnemonic::from_str(&my_words);
+
+    match result {
+        Ok(words) =>  {
+            println!("{}", words.to_string());
+
+            let seed = words.to_seed("");
+            println!("Seed -> {:?}", seed);
+
+            for i in 0..n {
+                let path = format!("m/44'/501'/{}'/0'", i);
+                let derivation_path = derive_from_path(&path, &seed);
+                let key_pair = Keypair::new_from_array(derivation_path.0);
+                let public_key = key_pair.pubkey();
+                let secret_key = key_pair.to_base58_string();
+
+                println!("Private Key -> {}", secret_key);
+                println!("Pulic Key -> {}", public_key);
+            }
+        }
+        Err(e) => println!("Error in generating words {:?}", e)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,9 @@
-use bip39::Language;
-use ed25519_hd_key::derive_from_path;
-use solana_sdk::{signature::Keypair, signer::Signer};
-
 mod raw_key_pairs;
+mod hd_wallet_sol;
 
 #[tokio::main]
 async fn main() {
     raw_key_pairs::sol_keys();
     raw_key_pairs::eth_keys().await;
-
-    let result = bip39::Mnemonic::generate_in(Language::English, 12);
-    match result {
-        Ok(words) =>  {
-            println!("{}", words.to_string());
-
-            let seed = words.to_seed("");
-            println!("Seed -> {:?}", seed);
-
-            for i in 0..1 {
-                let path = format!("m/44'/501'/{}'/0'", i);
-                let derivation_path = derive_from_path(&path, &seed);
-                let secret_key = bs58::encode(derivation_path.0).into_string();
-                let key_pair = Keypair::new_from_array(derivation_path.0);
-                let public_key = key_pair.pubkey();
-
-                println!("Private Key -> {}", secret_key);
-                println!("Pulic Key -> {}", public_key);
-            }
-        }
-        Err(e) => println!("Error in generating words {:?}", e)
-    }
+    hd_wallet_sol::generate_n_key_pair(1);
 }
